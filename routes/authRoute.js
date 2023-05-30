557dca193e730b59ccb0967765b8ee157eb5df94
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2023-05-30 17:26:02 +0530

    chnging some fixes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,40 +4,45 @@ const userModel = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const {audiLog, userIntel,ipIntel,domainIntel, clientIpAddress,hostIpAddress} = require('../pangea/pangea');
+// const requestIp = require('request-ip');
 router.post("/createuser", async (req,res)=>{
     const {email,name,password} = req.body;
     const findUser = await userModel.findOne({email : email});
     let msg;
     let description;
-    console.log(clientIpAddress(req));
-
+    // console.log(clientIpAddress(req));
+    const ip_address = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    console.log(ip_address);
+    console.log("another format");
+    // const clientIp = requestIp.getClientIp(req); 
+    // console.log(clientIp);
     //embargo
     // const responseEmbargo = await embargo.ipCheck(`${clientIpAddress(req)}`);
     // console.log(responseEmbargo);
     //geo locate
-    const ipIntelResponse = await ipIntel.geolocate(
-        `${clientIpAddress(req)}`,
-        {
-          provider: "digitalelement"
-        }
-    )
-    console.log(ipIntelResponse);
-    //is proxy
-    const responseIsproxy = await ipIntel.isProxy(
-        `${clientIpAddress(req)}`,
-        {
-          provider: "digitalelement"
-        }
-      );
-      console.log(responseIsproxy);
-      //is vpn
-      const responseVpn = await ipIntel.isVPN(
-        `${clientIpAddress(req)}`,
-        {
-          provider: "digitalelement"
-        }
-      );
-      console.log(responseVpn);
+    // const ipIntelResponse = await ipIntel.geolocate(
+    //     `${clientIpAddress(req)}`,
+    //     {
+    //       provider: "digitalelement"
+    //     }
+    // )
+    // console.log(ipIntelResponse);
+    // //is proxy
+    // const responseIsproxy = await ipIntel.isProxy(
+    //     `${clientIpAddress(req)}`,
+    //     {
+    //       provider: "digitalelement"
+    //     }
+    //   );
+    //   console.log(responseIsproxy);
+    //   //is vpn
+    //   const responseVpn = await ipIntel.isVPN(
+    //     `${clientIpAddress(req)}`,
+    //     {
+    //       provider: "digitalelement"
+    //     }
+    //   );
+    //   console.log(responseVpn);
     if (findUser) {
         // user found ask to log in in msg
         msg = "user found";
@@ -65,7 +70,7 @@ router.post("/createuser", async (req,res)=>{
             //means this email is breached
             // total no of breach = response.result.data.breach_count = 1
         }
-        console.log(response);
+        // console.log(response);
         const newUser = new userModel({email,name,hashedPassword});
         await newUser.save();
         req.session.userID = newUser._id;
